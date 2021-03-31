48a91535aa204ed5be7172f1e7941bd081733424
Emma Jones <emma@gracejones.email>
2021-03-31 02:19:16 -0500

    remove .history folder

--- .history/src/components/HelloWorld_20210329194443.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>The Golden Miner</h1>
-</template>
--- .history/src/main_20210330000629.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import VTooltip from "v-tooltip"
-
-createApp(App).use(VTooltip).mount('#app')
